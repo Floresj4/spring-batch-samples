074bf51a9267465d0a158b5622454cb81d577a31
Jason Flores <floresj4@gmail.com>
2022-07-14 20:42:14 -0400

    adding simple model

---
+++ src/main/java/com/flores/developmenet/springbatch/model/Person.java
@@ -0,0 +1,31 @@
+package com.flores.developmenet.springbatch.model;
+
+public class Person {
+
+	private String firstName;
+	private String lastName;
+	
+	public Person() {
+	}
+	
+	public Person(String first, String last) {
+		this.firstName = first;
+		this.lastName = last;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
