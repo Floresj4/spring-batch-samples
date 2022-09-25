e5b3a1d4664ff9b160d27e3d93cc84d5fbb3b27f
Jason Flores <floresj4@gmail.com>
2022-09-24 21:17:22 -0400

    adding job and step builder impl.

--- src/main/java/com/flores/developmenet/springbatch/config/BatchConfiguration.java
+++ src/main/java/com/flores/developmenet/springbatch/config/BatchConfiguration.java
@@ -2,9 +2,13 @@ package com.flores.developmenet.springbatch.config;
 
 import javax.sql.DataSource;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
@@ -29,6 +33,9 @@ public class BatchConfiguration {
 	@Autowired
 	private StepBuilderFactory stepBuilder;
 
+	@Autowired
+	private DataSource dataSource;
+	
 	@Bean
 	public FlatFileItemReader<Person> reader() {
 		return new FlatFileItemReaderBuilder<Person>()
@@ -48,12 +55,32 @@ public class BatchConfiguration {
 	}
 	
 	@Bean
-	public JdbcBatchItemWriter<Person> writer(DataSource dataSource) {
+	public JdbcBatchItemWriter<Person> writer() {
 		return new JdbcBatchItemWriterBuilder<Person>()
 				.itemSqlParameterSourceProvider(
 						new BeanPropertyItemSqlParameterSourceProvider<>())
 				.sql("INSERT INTO people (first_name, last_name) VALUES (:firstName, :lastName)")
 				.dataSource(dataSource)
 				.build();
+	}
+	
+	@Bean
+	public Step step(ItemWriter<Person> writer) {
+		return stepBuilder.get("step 1")
+			.<Person, Person>chunk(10)
+			.reader(reader())
+			.processor(processor())
+			.writer(writer())
+			.build();
+	}
+
+	@Bean
+	public Job importUserJob(JobCompletionNotificationListener listener, Step step) {
+		return jobBuilder.get("importUserJob")
+			.incrementer(new RunIdIncrementer())
+			.listener(listener)
+			.flow(step)
+			.end()
+			.build();			
 	}
 }
