6fba5fdfdd36ac2e68b5f6a34a005bb78906e1ca
Jason Flores <floresj4@gmail.com>
2022-09-24 21:29:31 -0400

    adding entrypoint

---
+++ src/main/java/com/flores/developmenet/springbatch/SpringBatchJdbc.java
@@ -0,0 +1,12 @@
+package com.flores.developmenet.springbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchJdbc {
+
+	public static void main(String args[]) {
+		SpringApplication.run(SpringBatchJdbc.class, args);
+	}
+}
