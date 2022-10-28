e28865c2352d3d709b59464fbd41e543fd1b4076
Jason Flores <floresj4@gmail.com>
2022-10-28 15:19:51 -0400

    adding employee data and model

---
+++ custom-record-parsing/src/main/java/com/flores/development/springbatch/model/Employee.java
@@ -0,0 +1,13 @@
+package com.flores.development.springbatch.model;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+
+	private int id;
+	private int deptId;
+	
+	private String name;
+	private String title;
+}
