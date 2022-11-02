03fc7a6c8eb0f39a868dc6e326bd17f8190e50ea
Jason Flores <floresj4@gmail.com>
2022-11-01 21:05:37 -0400

    creating work item class

---
+++ custom-record-parsing/src/main/java/com/flores/development/springbatch/model/WorkItem.java
@@ -0,0 +1,47 @@
+package com.flores.development.springbatch.model;
+
+import lombok.Getter;
+
+/**
+ * Simple work item consisting of an id
+ * and title.  Lombok.getter is used to generate
+ * the accessor methods for brevity.
+ * @author jason
+ */
+@Getter
+public class WorkItem {
+
+	private final int id;
+
+	private final String title;
+	
+	private WorkItem(WorkItemBuilder builder) {
+		this.id = builder.id;
+		this.title = builder.title;
+	}
+	
+	public static WorkItemBuilder builder() {
+		return new WorkItemBuilder();
+	}
+	
+	public static class WorkItemBuilder {
+
+		private int id;
+		
+		private String title;
+
+		public WorkItemBuilder withId(int id) {
+			this.id = id;
+			return this;
+		}
+		
+		public WorkItemBuilder withTitle(String title) {
+			this.title = title;
+			return this;
+		}
+		
+		public WorkItem build() {
+			return new WorkItem(this);
+		}
+	}
+}
