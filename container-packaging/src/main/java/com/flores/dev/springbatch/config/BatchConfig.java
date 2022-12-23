6781000efa0cea0fa23b3fba621200608073443d
Jason Flores <floresj4@gmail.com>
2022-12-22 21:23:13 -0500

    completing batch processor

--- container-packaging/src/main/java/com/flores/dev/springbatch/config/BatchConfig.java
+++ container-packaging/src/main/java/com/flores/dev/springbatch/config/BatchConfig.java
@@ -3,7 +3,11 @@ package com.flores.dev.springbatch.config;
 import java.io.File;
 import java.io.FileNotFoundException;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemStreamReader;
 import org.springframework.batch.item.ItemStreamWriter;
@@ -11,6 +15,7 @@ import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,13 +30,16 @@ import lombok.extern.slf4j.Slf4j;
 @Configuration
 @EnableBatchProcessing
 public class BatchConfig {
-
-	@Value("#{jobParameters['inputFile]}") 
-	public String inputFile;
+	
+	@Autowired
+	private StepBuilderFactory stepBuilder;
+	
+	@Autowired
+	public JobBuilderFactory jobBuilder;
 	
 	@Bean
 	@StepScope
-	public ItemStreamReader<Person> reader() {
+	public ItemStreamReader<Person> reader(@Value("#{jobParameters['inputFile']}") String inputFile) {
 		log.info("Initializing input file {}", inputFile);
 		
 		Resource fileResource = new FileSystemResource(inputFile);
@@ -49,9 +57,9 @@ public class BatchConfig {
 	
 	@Bean
 	@StepScope
-	public ItemStreamWriter<Person> write() throws Exception {
+	public ItemStreamWriter<Person> writer(@Value("#{jobParameters['inputFile']}") String inputFile) throws Exception {
 		
-		File outputFile = getOutputFile();
+		File outputFile = getOutputFile(inputFile);
 		Resource fileResource = new FileSystemResource(outputFile);
 
 		return new FlatFileItemWriterBuilder<Person>()
@@ -63,8 +71,31 @@ public class BatchConfig {
 				})
 				.build();
 	}
-	
-	private File getOutputFile() throws Exception {
+
+	@Bean
+	public Step batchProcessingStep() throws Exception {
+		return stepBuilder
+				.get("PersonProcessingStep")
+				.<Person, Person>chunk(5)
+				.reader(reader(null))
+				.writer(writer(null))
+				.build();
+	}
+
+	@Bean
+	public Job batchProcessingJob() throws Exception {
+		return jobBuilder
+				.get("PersonProcessingJob")
+				.flow(batchProcessingStep())
+				.end()
+				.build();
+	}
+
+	/**
+	 * @return file to write based on the input file
+	 * @throws Exception on initializing file
+	 */
+	private File getOutputFile(String inputFile) throws Exception {
 		File input = new File(inputFile);
 		if(!input.exists()) {
 			throw new FileNotFoundException("Input file does not exist.");
@@ -81,11 +112,9 @@ public class BatchConfig {
 		
 		String name = input.getName();
 		String extension = name.substring(separator + 1);
-		String outName = name
-				.substring(0, separator)
-				.replace(extension, "-out." + extension);
+		String outName = name.substring(0, separator);
 		
-		File outputFile = new File(parent, outName);
+		File outputFile = new File(parent, outName + "-out." + extension);
 		
 		log.info("Initializing output file as {}", outputFile.getCanonicalPath());
 		return outputFile;
