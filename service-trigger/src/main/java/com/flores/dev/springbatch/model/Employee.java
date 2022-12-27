c217752892eb9116eea88d04533a821af5441f48
Jason Flores <floresj4@gmail.com>
2022-12-26 21:52:40 -0500

    adding models

---
+++ service-trigger/src/main/java/com/flores/dev/springbatch/model/Employee.java
@@ -0,0 +1,24 @@
+package com.flores.dev.springbatch.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+	
+	private String id;
+	private String name;
+	private String deptId;
+	private String title;
+	private String dateOfBirth;
+	
+	private List<WorkItem> items = new ArrayList<>();
+}
