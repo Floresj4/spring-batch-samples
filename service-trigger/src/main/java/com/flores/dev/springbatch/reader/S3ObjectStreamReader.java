51668a2d643561eca0ad9577caf476f116b2f150
Jason Flores <floresj4@gmail.com>
2022-12-29 10:20:57 -0500

    adding s3 and creating stream reader

---
+++ service-trigger/src/main/java/com/flores/dev/springbatch/reader/S3ObjectStreamReader.java
@@ -0,0 +1,167 @@
+package com.flores.dev.springbatch.reader;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Optional;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.item.ItemStreamException;
+import org.springframework.batch.item.ItemStreamReader;
+import org.springframework.batch.item.json.JacksonJsonObjectReader;
+import org.springframework.batch.item.json.builder.JsonItemReaderBuilder;
+import org.springframework.core.io.InputStreamResource;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.flores.dev.springbatch.model.Employee;
+
+import lombok.extern.slf4j.Slf4j;
+import software.amazon.awssdk.core.ResponseInputStream;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+
+/**
+ * I had to create a builder manually because lombok doesn't
+ * let you hide fields
+ * 
+ * @author jason
+ */
+@Slf4j
+public class S3ObjectStreamReader implements ItemStreamReader<Employee> {
+
+	private String resource;
+	
+	private S3Client client;
+
+	private ItemStreamReader<Employee> delegate;
+
+	private ResponseInputStream<GetObjectResponse> response;
+
+	private final Lock lock = new ReentrantLock();
+
+	private S3ObjectStreamReader(S3ObjectStreamReaderBuilder builder) {
+		this.resource = builder.resource;
+		this.client = builder.client;
+	}
+
+	@PostConstruct
+	public void init() throws URISyntaxException {
+		delegate = getDelegate();
+	}
+
+	@Override
+	public void open(ExecutionContext executionContext) throws ItemStreamException {
+		delegate.open(executionContext);
+	}
+
+	@Override
+	public void update(ExecutionContext executionContext) throws ItemStreamException {
+		delegate.update(executionContext);
+	}
+
+	@Override
+	public void close() throws ItemStreamException {
+		//close the response before closing the entire reader
+		try { response.close(); } catch (IOException e) {
+			log.error("Unable to close response on StreamReader.close()");
+		}
+		
+		client.close();
+		delegate.close();
+	}
+
+	/**
+	 * get the GetObjectRequest from a resource path
+	 * @param path uri string
+	 * @return a S3 GetObjectRequest
+	 * @throws URISyntaxException on path errors
+	 */
+	public static GetObjectRequest getObjectRequestFromResourcePath(String path) throws URISyntaxException {
+		URI uri = new URI(path);
+		String bucket = uri.getHost();
+		String key = uri.getPath().substring(1);
+
+		log.debug("S3 GetObjectRequest bucket and key from {} as {} : {}",
+				path, bucket, key);
+
+		return GetObjectRequest.builder()
+				.bucket(bucket)
+				.key(key)
+				.build();
+	}
+
+	/**
+	 * Fetch the S3 Object input stream and set on JsonItemReader
+	 * @return an ItemStream reader from the remote object input stream
+	 * @throws URISyntaxException on bad path
+	 */
+	private ItemStreamReader<Employee> getDelegate() throws URISyntaxException {
+		if(Optional.ofNullable(delegate).isEmpty()) {			
+			log.info("Initializing remote resource reader for {}", resource);
+
+			//request object
+			GetObjectRequest request = getObjectRequestFromResourcePath(resource);
+			response = client.getObject(request);
+
+			//setup reader with customizable mapper
+			ObjectMapper mapper = new ObjectMapper();
+			JacksonJsonObjectReader<Employee> reader = new JacksonJsonObjectReader<>(mapper, 
+					Employee.class);
+
+			delegate = new JsonItemReaderBuilder<Employee>()
+					.resource(new InputStreamResource(response))
+					.name("StreamedEmployeeJsonReader")
+					.jsonObjectReader(reader)
+					.build();
+		}
+		
+		return delegate;
+	}
+	
+	@Override
+	public Employee read() throws Exception {
+		lock.lock();
+		
+		try {
+			delegate = getDelegate();
+			return delegate.read();
+		}
+		finally {
+			lock.unlock();
+		}
+	}
+	
+	public static S3ObjectStreamReaderBuilder builder() {
+		return new S3ObjectStreamReaderBuilder();
+	}
+
+	public static class S3ObjectStreamReaderBuilder {
+
+		private String resource;
+		
+		private S3Client client;
+		
+		S3ObjectStreamReaderBuilder() {
+			
+		}
+		
+		public S3ObjectStreamReaderBuilder withResource(String resource) {
+			this.resource = resource;
+			return this;
+		}
+		
+		public S3ObjectStreamReaderBuilder withClient(S3Client client) {
+			this.client = client;
+			return this;
+		}
+		
+		public S3ObjectStreamReader build() {
+			return new S3ObjectStreamReader(this);
+		}
+	}
+}
