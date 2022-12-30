aa8dda6ba5a8c998e1769fae5108ebd1648f2f90
Jason Flores <floresj4@gmail.com>
2022-12-30 09:28:38 -0500

    clean up

--- service-trigger/src/main/java/com/flores/dev/springbatch/config/JsonReadConfig.java
+++ service-trigger/src/main/java/com/flores/dev/springbatch/config/JsonReadConfig.java
@@ -13,8 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 
 import com.flores.dev.springbatch.model.Employee;
