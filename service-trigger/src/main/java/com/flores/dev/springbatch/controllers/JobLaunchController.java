78fa7f800b34ca488606ab540c6aacbc04c29b41
Jason Flores <floresj4@gmail.com>
2022-12-31 11:48:16 -0500

    change controller name

--- service-trigger/src/main/java/com/flores/dev/springbatch/controllers/JobLaunchController.java
+++ service-trigger/src/main/java/com/flores/dev/springbatch/controllers/JobLaunchController.java
@@ -25,7 +25,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
-public final class ServiceJobLauncher {
+public final class JobLaunchController {
 
 	@Autowired
 	private SimpleJobLauncher jobLauncher;
