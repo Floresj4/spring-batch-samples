d657ba226aaeb12690fe344bb03a859e155a03bd
Jason Flores <floresj4@gmail.com>
2025-01-19 19:34:45 -0500

    updating pom for spring dependencies

---
+++ spring-dynamo/src/main/java/com/flores/dev/springbatch/App.java
@@ -0,0 +1,11 @@
+package com.flores.dev.springbatch;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String args[]) throws Exception {
+		
+	}
+}
